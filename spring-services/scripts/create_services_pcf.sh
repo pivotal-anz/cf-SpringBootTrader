422be80d265b3c2f114573d7672be3ad1b64b21b
fushewokunze-pivotal <fushewokunze@pivotail.io>
2015-12-09 20:01:04 +1100

    clean up unused files

--- spring-services/scripts/create_services_pcf.sh
+++
@@ -1,4 +0,0 @@
-cf cs p-mysql 100mb-dev fortune-db
-cf cs p-config-server standard config-service
-cf cs p-service-registry standard service-registry
-cf cs p-circuit-breaker-dashboard standard circuit-breaker
